cc73f2ca5f0b021492534e0c2eba20a540dd106b
LordTrua <70875440+LordTrua@users.noreply.github.com>
2020-12-16 17:34:14 +0300

    Add files via upload

---
+++ LabRabota № 7/LabRabota № 7/Workers and Department.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LabRabota___7
+{
+    class Workers_and_Department
+    {
+        Department Dep; //Department id
+        Workers Wor;// Worker id
+        public Workers_and_Department(Workers W, Department De)
+        {
+            Wor = W;
+            Dep = De;
+        }
+        public Workers get_W() { return this.Wor; }
+        public Department get_D() { return this.Dep; }
+    }
+}
