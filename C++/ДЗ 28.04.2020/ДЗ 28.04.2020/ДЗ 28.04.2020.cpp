f1a5f27c0c1e72f5724d91e28bfa4b834ae77dbc
LordTrua <70875440+LordTrua@users.noreply.github.com>
2021-11-07 01:10:16 +0300

    Add files via upload

---
+++ C++/ДЗ 28.04.2020/ДЗ 28.04.2020/ДЗ 28.04.2020.cpp
@@ -0,0 +1,57 @@
+﻿// ДЗ 28.04.2020.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Выполнил: Сафиуллин А.П. РТ5-21
+
+#include <iostream>
+#include<cmath>
+#include <Windows.h>
+#include <string>
+
+using namespace std;
+class complex 
+{
+public:
+    int cel, comp;
+    complex(int c, int d)
+    {
+        cel = c;
+        comp = d;
+    }
+    void display()
+    {
+        cout << "Число: " << cel << "+" << comp << "i" << endl;
+    }
+};
+
+complex operator + (complex c1, complex c2)
+{
+   cout << "Сложение: " << endl;
+  int  c0 = c1.cel + c2.cel;
+   int d0 = c1.comp + c2.comp;
+   return complex(c0, d0);
+}
+complex operator *(complex c1, complex c2)
+{
+    cout << "Умножение: " << endl;
+   int c0 = (c1.cel * c2.cel) - (c1.comp * c2.comp);
+   int d0 = (c1.cel * c2.comp) + (c2.cel * c1.comp);
+   return complex(c0, d0);
+}
+
+int main()
+{
+    setlocale(0, "rus");
+    int a, b, c, d;
+    cout << "Введите целую часть 1го числа: "; cin >> c;
+    cout << "Введите комплексную часть 1го числа: "; cin >> d;
+    complex c1(c, d);
+    c1.display();
+    cout << "Введите целую часть 2го числа: "; cin >> a;
+    cout << "Введите комплексную часть 2го числа: "; cin >> b;
+    complex c2(a, b);
+    c2.display();
+    complex c3 = c1 + c2;
+    c3.display();
+    complex c4 = c1 * c2;
+    c4.display();
+}
+
---
+++ C++/ДЗшка/ДЗшка/ДЗшка.cpp
@@ -0,0 +1,133 @@
+﻿// ДЗшка.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+/*
+
+Родительский класс бронемашина , развился дочерний классОБТ,
+обладающий св-вами бронемашины и своими качествами, но вместе с ним появилась универсальна
+боевая платформа, база для узких решений(САУ(с усиленным орудием), БМП(танк с дес. отделением),
+БТР для борьбы с танками, для инженерных войск и т.д. и т.д.
+*/
+
+#include <iostream>
+#include<string>
+#include<Windows.h>
+
+
+using namespace std;
+
+class Panzerwagen
+{
+public:
+    string Armor;
+    string Gun;
+    string name;
+    string Classname;
+    Panzerwagen(string n)
+    {
+        name = n;
+        Armor = "Есть броня";
+        Gun = "Орудие или пулемёт";
+        Classname = "Танкетка/ броневик";
+    }
+    Panzerwagen(string n,string CN)
+    {
+        name = n;
+        Armor = "Есть броня";
+        Gun = "Орудие и пулемёт";
+        Classname = CN;
+    }
+   
+    void display()
+    {
+        cout << "Название техники: " << name << "\nКласс: " << Classname << "\nБроня: " << Armor << "\nВооружение: " << Gun << endl<<endl;
+    }
+};
+class Panzers : public Panzerwagen
+{
+public:
+    string Bulletprot;
+    string shellprot;
+    string EngineType;
+    Panzers(string n, string CN) : Panzerwagen(n, CN)
+    {
+        Bulletprot = "Противопульное бронирование";
+        shellprot = "Противоснарядное бронирование";
+        EngineType = "Дизель";
+    }
+    Panzers(string n, string CN, string ET) : Panzerwagen(n, CN)
+    {
+        Bulletprot = "Противопульное бронирование";
+        shellprot = "Противоснарядное бронирование";
+        EngineType = ET;
+    }
+    void display()
+    {
+        cout << "Название техники: " << name << "\nКласс: " << Classname << "\nБроня: " << Armor << ": " << Bulletprot << "," << shellprot << "\nВооружение: " << Gun<<"\nТип двигателя:"<<EngineType<<endl<<endl;
+
+    }
+
+};
+class OBT : public Panzers
+{
+public:
+    string actprot;
+    string platf;
+     string Gunz;
+    OBT(string n, string CN, string ET, int plat) : Panzers(n, CN, ET)
+    {
+        actprot = "Активная защита";
+        Gunz = Gunt;
+        if (plat == 1)
+        {
+            platf = "Да";
+
+        }
+        else if (plat == 0)
+        {
+            platf = "нет";
+        }
+    }
+    OBT(string n, string CN, string ET,int plat,string GZ) : Panzers(n, CN, ET)
+    {
+        actprot = "Активная защита";
+        Gunz = GZ;
+        platf = "Да";
+    }
+
+    void display()
+    {
+        cout << "Название техники: " << name << "\nКласс: " << Classname << "\nБроня: " << Armor << ": " << Bulletprot << "," << shellprot<<","<< actprot << "\nВооружение: " << Gunz << "\nТип двигателя:" << EngineType <<"\nНа базе универсальной платформы: "<<platf<< endl<<endl;
+    }
+private:
+    string Gunt="Универсальное орудие с многоцелевым снарядом";
+};
+class Platform : public OBT
+{
+public:
+    Platform(string n, string CN, string ET, string GZ,string MT) : OBT(n, CN, ET, 1, GZ)
+    {
+        MachineType = MT;
+    }
+    void display()
+    {
+        cout << "Название техники: " << name << "\nКласс: " << Classname << "\nБроня: " << Armor << ": " << Bulletprot << "," << shellprot << "," << actprot << "\nВооружение: " << Gunz << "\nТип двигателя:" << EngineType << "\nНа базе универсальной платформы: " << platf<<"\nНазначение машины: "<<MachineType << endl << endl;
+    }
+private:
+    string  MachineType;
+};
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    Panzerwagen BA10 ("Ба-10");
+    BA10.display();
+    Panzers Pz4("PzKpfw IV", "Танк");
+    Pz4.display();
+    OBT Armata("T-14 (Армата)", "ОБТ", "Универсальный", 1);
+    Armata.display();
+    Platform ArmataТ("T-15", "ОБТ", "Универсальный","Крупнокалиберный пулемёт","БТР");
+    ArmataТ.display();
+    OBT Abrams("Abrams", "ОБТ", "Универсальный", 0);
+    Abrams.display();
+}
+
+
