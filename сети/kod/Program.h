ee411d0970c396dff6134dc61d598dcfd2910ffb
LordTrua <70875440+LordTrua@users.noreply.github.com>
2021-11-13 20:20:26 +0300

    Add files via upload

---
+++ сети/kod/Program.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include <string>
+#include <vector>
+#include <iostream>
+#include <cmath>
+#include "stringhelper.h"
+
+namespace kod
+{
+	class Program
+	{
+	public:
+		static void Main(std::vector<std::wstring> &args);
+	};
+}
---
+++ сети/kod/Properties/AssemblyInfo.h
@@ -0,0 +1,58 @@
+﻿#pragma once
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyTitle("kod")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyDescription("")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyConfiguration("")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyCompany("")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyProduct("kod")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyCopyright("Copyright ©  2015")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyTrademark("")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyCulture("")]
+
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: ComVisible(false)]
+
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: Guid("3b59b4c8-84c4-47df-b0db-ab7ea2e5db6d")]
+
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//      Номер построения
+//      Редакция
+//
+// Можно задать все значения или принять номер построения и номер редакции по умолчанию, 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyVersion("1.0.0.0")]
+
+//C# TO C++ CONVERTER NOTE: The following .NET attribute has no direct equivalent in C++:
+//ORIGINAL LINE: [assembly: AssemblyFileVersion("1.0.0.0")]
+
